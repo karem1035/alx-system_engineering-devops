6bd237f0f859a8311418c5c1e0007e5a07dc1e26
Karem Darwesh <karemdarwesh1035@gmail.com>
2024-05-07 18:56:21 +0300

    cp the task to check

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,12 +7,14 @@ import requests
 def number_of_subscribers(subreddit):
     """
     Function that queries the Reddit API
+    - If not a valid subreddit, return 0.
     """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
 
-    res = requests.get(
-        f'https://www.reddit.com/r/{subreddit}/about.json',
-        headers={"User-Agent": "Custom"})
-    if res.status_code != 200:
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
+    else:
         return 0
-    data = res.json().get('data').get('subscribers')
-    return (data)
