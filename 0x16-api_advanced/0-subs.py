ddd37b2c00973230c02d9839d89c8ba53e02a389
Karem Darwesh <karemdarwesh1035@gmail.com>
2024-05-07 18:43:20 +0300

    task 0 style

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """ returns the number of subscribers for a given subreddit. """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """
+    Function that queries the Reddit API
+    """
+
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     res = requests.get(url)
     if res.status_code != 200:
         return 0
-    data = res.json()['data']
-    return (data['subscribers'])
+    data = res.json().get('data').get('subscribers')
+    return (data)
